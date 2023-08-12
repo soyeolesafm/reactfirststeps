870217099f33df3d458710de9e7eee43817229d9
soyeolesafm <soyelesafm@hotmail.com>
2023-08-12 15:42:22 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import { useState } from "react"
+import './App.css'
+
+// ! importar usestate para poder usarlo dento de nuestra 
+
+
+const App = () => {
+  
+  // useState se utiliza como un array en el que declaras la una variable state "count"
+  // que retiene la data y un state setter "setCount" que actualiza el valor de la variable
+  //! ejemplo: 
+  //* const [count, setCount] = useState(0)
+  //* const increment = () => {
+  // *  setCount ( count + 1 )
+  //* }
+  
+  const [ darkMode, setDarkMode ] = useState(false)
+
+  const modeToggler = () => {
+    setDarkMode ( !darkMode )
+  }
+
+  return(
+    <div className= { darkMode ? " dark_mode app " : "app" }  >
+      <h1>Hello React!</h1>
+      <button className="btn" onClick={modeToggler} > Click </button>
+      <h1> { darkMode ? "🌜 dark" : "🌞 light" } </h1>
+    </div>
+  )
+}
+
+
+export default App
